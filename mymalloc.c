3db3ebdbaed061d3d30c7139dbe280e360117bc5
leocelii <147785458+leocelii@users.noreply.github.com>
2024-02-19 12:09:38 -0500

    Update mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -17,8 +17,8 @@ typedef struct Node {
 } chunkNode;
 
 void initializeMemory() {
-    chunkNode *memStart = (chunkNode *)memory;
-    memStart->size = MEMSIZE;
+    chunkNode *memStart = (chunkNode*)memory;
+    memStart->size = MEMSIZE - HEADERSIZE;
     memStart->allocated = 0;
 }
 void *mymalloc(size_t size, char *file, int line){
@@ -27,31 +27,29 @@ void *mymalloc(size_t size, char *file, int line){
     }
 
     //if memory array has not been intialized, and mymalloc is called for the first time, then this step will initialize the memory array
-    static int initialized = 0; //static makes it so, it retains its value as 1, during every call to malloc, after the first.
-    if (!initialized) {
+    chunkNode *memoryInitialized = (chunkNode*)memory;
+    if (memoryInitialized->size <= 0 || memoryInitialized->size > MEMORY_SIZE || memoryInitialized->allocated != 0 && memoryInitialized->allocated != 1) {
     initializeMemory(); //initialize memory array;
-    initialized = 1; //subsequent calls to mymalloc will already have the memory array initialized, so this step will be skipped. 
     }
     
     int newByteSize = (size + 7) & -7; //ensures that requested size is rounded up to the nearest multiple of 8
-    size = (newByteSize < 16) ? 16: size; //ensures that the size is at least minimum chunk size of 16, 8 min for metadata, 8 min. for payload
+    size = (newByteSize < 16) ? 16 : newByteSize; //ensures that the size is at least minimum chunk size of 16, 8 min for metadata, 8 min. for payload
     
-    char *memoryStart = (char *)memory; //setup for byte-width pointer arithmetic
+    char *memoryStart = (char*)memory; //setup for byte-width pointer arithmetic
     while (memoryStart < ((char*)memory + MEMSIZE)) { //within range of memory
-        chunkNode *currentNode = (chunkNode *)memoryStart; //memoryStart gets casted to a pointer of type chunkNode to access the metadata at the current chunk
-        if(currentNode->allocated == 0 && currentNode->size >= size){ 
-            //if the current chunk's metadata represents a free chunk, whose size is greater than/equalto the requested size, than the requested chunk has been found
-            if (currentNode->size > size) { 
-                chunkNode *newNode = (chunkNode *)(memoryStart + size);
-                newNode->size = currentNode->size - size;
+        chunkNode *currentNode = (chunkNode*)memoryStart; //memoryStart gets casted to a pointer of type chunkNode to access the metadata at the current chunk
+        if(currentNode->allocated == 0 && currentNode->size >= size){ //if the current chunk's metadata represents a free chunk, whose size is greater than/equalto the requested size, than the requested chunk has been found
+            if (currentNode->size > size && ((currentNode->size - size - HEADERSIZE) >= HEADERSIZE)) {  //if possible, split into two chunks
+                chunkNode *newNode = (chunkNode *)(memoryStart + size + HEADERSIZE);
+                newNode->size = currentNode->size - size - HEADERSIZE;
                 newNode->allocated = 0;
                 currentNode->size = size;
             }
             currentNode->allocated = 1;
-            return memoryStart + sizeof(chunkNode);
+            return memoryStart + HEADERSIZE;
     
         }
-        memoryStart += ((currentNode->size));
+        memoryStart += ((currentNode->size) + HEADERSIZE);
     }
     return NULL; //if no space is found that matches the requested amount of bytes, return NULL or "space unavailable"
 }

887e6c523066b7d5f4f380fd1d4c17fa5f40bb1d
leocelii <147785458+leocelii@users.noreply.github.com>
2024-02-20 18:22:51 -0500

    Update (test)nonAllocatedAddress.c

--- (test)nonAllocatedAddress.c
+++ (test)nonAllocatedAddress.c
@@ -5,6 +5,6 @@ int main(){
     //attepmt to free address of non-allocated object
     int *p = malloc(sizeof(int));
     free(p); // free the memory
-    free(p); // attemps to free p again(should not work since memory was already allocated)
+    free(p); // attemps to free p again(should not work since memory was already unallocated)
     return 0;
 }

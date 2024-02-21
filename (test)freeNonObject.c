6ba94eec658db367614fb6462473899ff63c98e0
leocelii <147785458+leocelii@users.noreply.github.com>
2024-02-20 19:31:22 -0500

    Update (test)freeNonObject.c

--- (test)freeNonObject.c
+++ (test)freeNonObject.c
@@ -6,4 +6,5 @@ int main() {
     // attempts to free address in the heap but not of an object
     void *ptr = malloc(10); // this allocates memory
     free(ptr +5); //attempts to free object in the middle of the allocated memory
+    return 0;
 }
